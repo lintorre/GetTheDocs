572ced98158f462a600099886c8181b70d923b98
lintorre <50646759+lintorre@users.noreply.github.com>
2019-05-14 23:23:01 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template_new'
-copyright = u'2014, Read the Docs'
+project = u'Read the Docs - Engineering Demo'
+#copyright = u'2014, Read the Docs'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
